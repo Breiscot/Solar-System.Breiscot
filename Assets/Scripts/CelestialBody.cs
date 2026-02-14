6e1e943aa5a9f2913f3643e36e41c99e0b92fdf7
Breiscot <omegaxgaming09@gmail.com>
2026-02-14 10:26:34 +0100

    first commit

---
+++ Assets/Scripts/CelestialBody.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CelestialBody : MonoBehaviour
+{
+    [Header("Proprietà Fisiche")]
+    public float mass = 1000f;
+    public float radius = if;
+
+    [Header("Velocità Iniziale")]
+    public Vector3 initialVelocity;
+
+    [Header("Visualizzazione")]
+    public Color orbitColor = Color.white;
+    public bool drawOrbitPath = true;
+
+    
+}
